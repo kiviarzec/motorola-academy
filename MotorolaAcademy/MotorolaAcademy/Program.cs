6159680d47f0437dfc21e19fa0af8dbc5cccdd6e
kiviarzec <kaerzet@op.pl>
2020-12-01 23:31:54 +0100

    add reading from countries_and_capitals file

---
+++ MotorolaAcademy/MotorolaAcademy/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace MotorolaAcademy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            // Read each line of the file into a string array. Each element
+            // of the array is one line of the file.
+            string[] lines = System.IO.File.ReadAllLines(@"C:\Users\kaerz\Documents\motorola-academy\countries_and_capitals.txt.txt");
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                Console.WriteLine(lines[i]);
+            }      
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+}
+    }
+}
