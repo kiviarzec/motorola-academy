e44caf34b2a1ccb6efdd55e17ecb7d32d3e60d0e
kiviarzec <kaerzet@op.pl>
2020-12-02 00:20:05 +0100

    Check user guess

--- MotorolaAcademy/MotorolaAcademy/Program.cs
+++ MotorolaAcademy/MotorolaAcademy/Program.cs
@@ -11,14 +11,14 @@ namespace MotorolaAcademy
             // of the array is one line of the file.
             string[] lines = System.IO.File.ReadAllLines(@"C:\Users\kaerz\Documents\motorola-academy\countries_and_capitals.txt.txt");
 
-//            for (int i = 0; i < lines.Length; i++)
-//            {
-//                Console.WriteLine(lines[i]);
-//            }
+            //            for (int i = 0; i < lines.Length; i++)
+            //            {
+            //                Console.WriteLine(lines[i]);
+            //            }
 
             // Instantiate random number generator using system-supplied value as seed.
             var random = new Random();
-            int randomIndex = random.Next(0,lines.Length-1);  // creates a number between 1 and 12
+            int randomIndex = random.Next(0, lines.Length - 1);  // creates a number between 1 and 12
 
             Console.WriteLine(randomIndex);
 
@@ -30,6 +30,18 @@ namespace MotorolaAcademy
             string capital = words[1];
             Console.WriteLine(country);
             Console.WriteLine(capital);
+
+            string guess = Console.ReadLine();
+
+            if (guess == capital)
+            {
+                Console.WriteLine("That's correct!");
+            }
+            else
+            {
+                Console.WriteLine("That's incorrect!");
+            }
+
 
 
 
