48f5817e9f355bc924a8296e0f781297d09779e8
kiviarzec <kaerzet@op.pl>
2020-12-02 23:16:57 +0100

    Add not-in-word list

--- MotorolaAcademy/MotorolaAcademy/Program.cs
+++ MotorolaAcademy/MotorolaAcademy/Program.cs
@@ -36,16 +36,25 @@ namespace MotorolaAcademy
             {
                 capitalAsChars[i] = '_';
             }
-            Console.WriteLine(capitalAsChars);
 
 
             int lives = 5;
             Console.WriteLine("You've got " + lives + " lives");
 
+            char[] notInWordList = new char[] { '_', '_', '_', '_', '_' };
+
+
             Console.WriteLine("Let's play!");
 
             while (lives > 0)
             {
+
+                Console.WriteLine("Capital to guess : ");
+                Console.WriteLine(capitalAsChars);
+
+                Console.WriteLine("Not in word list : ");
+                Console.WriteLine(notInWordList);
+
 
                 Console.WriteLine("Do you want to guess a single letter or the whole word?");
                 Console.WriteLine("Write LETTER or WORD to choose");
@@ -101,10 +110,6 @@ namespace MotorolaAcademy
                                 Console.WriteLine("That letter is in that word!");
 
                                 capitalAsChars[i] = guessLetter[0];
-
-                                Console.WriteLine(capitalAsChars);
-
-
                             }
 
                         }
@@ -113,6 +118,11 @@ namespace MotorolaAcademy
                     {
                         
                         Console.WriteLine("That letter isn't in that word!");
+
+                        notInWordList[5-lives] = guessLetter[0];
+
+
+
 
                         lives--;
                         Console.WriteLine("You've got " + lives + " lives");
@@ -124,6 +134,7 @@ namespace MotorolaAcademy
                         else
                         {
                             Console.WriteLine("Game over!");
+                            Console.WriteLine("It was a capitol of " + country);
                         }
                     }
                 }
