fd8a653b0d06abc16a18b2a1c7ca538cb74d3a85
kiviarzec <kaerzet@op.pl>
2020-12-03 16:17:43 +0100

    Remove unnnessesery lines

--- MotorolaAcademy/MotorolaAcademy/Program.cs
+++ MotorolaAcademy/MotorolaAcademy/Program.cs
@@ -16,30 +16,17 @@ namespace MotorolaAcademy
             do
             {
 
-                Console.WriteLine("Hello World!");
-                // Read each line of the file into a string array. Each element
-                // of the array is one line of the file.
                 string[] lines = System.IO.File.ReadAllLines(@"C:\Users\kaerz\Documents\motorola-academy\countries_and_capitals.txt.txt");
 
-                //            for (int i = 0; i < lines.Length; i++)
-                //            {
-                //                Console.WriteLine(lines[i]);
-                //            }
-
-                // Instantiate random number generator using system-supplied value as seed.
                 var random = new Random();
-                int randomIndex = random.Next(0, lines.Length - 1);  // creates a number between 1 and 12
-
-                Console.WriteLine(randomIndex);
+                int randomIndex = random.Next(0, lines.Length - 1); 
 
                 string randomLine = lines[randomIndex];
-                Console.WriteLine(randomLine);
 
                 string[] words = randomLine.Split(" | ");
                 string country = words[0].ToUpper();
                 string capital = words[1].ToUpper();
-                Console.WriteLine(country);
-                Console.WriteLine(capital);
+
                 char[] capitalAsChars = capital.ToCharArray();
                 Console.WriteLine(capitalAsChars);
                 for (int i = 0; i < capitalAsChars.Length; i++)
